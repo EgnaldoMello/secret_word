b18cad01a3a0d5a90628e705e032aff760db50bc
Antônio Egnaldo <egnaldomello@gmail.com>
2025-07-07 21:04:09 -0300

    3º commit - Finalizado a lógica do jogo em react

--- secret_word/secret_word/src/components/EndScreen.jsx
+++ secret_word/secret_word/src/components/EndScreen.jsx
@@ -1,7 +1,15 @@
-import "./EndScreen.css";
+import "./EndScreen.modules.css";
 
-const EndScreen = () => {
-  return <div>EndScreen</div>;
+const EndScreen = ({ retry, score }) => {
+  return (
+    <div>
+      <h1>Fim de jogo</h1>
+      <h2>
+        A sua pontuação foi: <span>{score}</span>
+      </h2>
+      <button onClick={retry}>Resetar jogo</button>
+    </div>
+  );
 };
 
 export default EndScreen;
