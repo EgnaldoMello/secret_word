84732eeb76dfff594e837df132469f89102532c8
EgnaldoMello <egnaldomello@gmail.com>
2025-07-22 21:48:38 -0300

    4º commit - adicionados os comentários sobre o código e resolvidos pequenos erros

--- secret_word/secret_word/src/App.jsx
+++ secret_word/secret_word/src/App.jsx
@@ -2,126 +2,146 @@
 import "./App.css";
 //react
 import { useCallback, useEffect, useState } from "react";
-//data
+//data - lista de palavras
 import { wordsList } from "./data/words";
 //components
 import StartScreen from "./components/StartScreen";
 import GameScreen from "./components/GameScreen";
 import EndScreen from "./components/EndScreen";
-
+//estágios do jogo
 const stages = [
   { id: 1, name: "start" },
   { id: 2, name: "game" },
   { id: 3, name: "end" },
 ];
-
+//as tentativas são 3
 const guessesQty = 3;
 
 const App = () => {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
-
   const [pickedWord, setPickedWord] = useState("");
   const [pickedCategory, setPickedCategory] = useState("");
   const [letters, setLetters] = useState([]);
+  //letras adivinhadas
   const [guessedLetters, setGuessedLetters] = useState([]);
+  //letras erradas
   const [wrongLetters, setWrongLetters] = useState([]);
+  //tentativas do usuário já setadas para 3
   const [guesses, setGuesses] = useState(guessesQty);
-  const [score, setScore] = useState(50);
-
+  const [score, setScore] = useState(0);
+  //useCallBack para evitar que a função seja executada várias vezes
   const pickWordAndCategory = useCallback(() => {
-    //pick a random category
+    //as categorias são as chaves do objeto wordList que está no useState words
     const categories = Object.keys(words);
+    //categories = é um array de todas as chaves. Category = pega uma chave aleatória
     const category =
       categories[Math.floor(Math.random() * Object.keys(categories).length)];
-
-    //pick a random word
+    //pegando uma palavra aleatória da categoria selecionada. O objeto precisa ser acessado por [] pois estamos utilizando uma constante que recebe a string de categories
     const word =
       words[category][Math.floor(Math.random() * words[category].length)];
-
+    //função retorna um objeto da palavra e da categoria selecionadas
     return { word, category };
   }, [words]);
 
-  //starts the game
+  //função para começar a jogar
+  //useCallBack para evitar que a função seja executada várias vezes
   const startGame = useCallback(() => {
-    //clear all letters
+    //função para limpar todas as letras
     clearLettersStates();
-    //pick word and pick category
+    //função para selecionar a palavra e a categoria é ativada, já começa com as constantes word e category desestruturadas e preenchidas
     const { word, category } = pickWordAndCategory();
-    //create an array of letters
+    //criando a sepração das letras da palavra selecionada através de um array
     let wordLetters = word.split("");
+    //colocando todos os itens do array em minúsculo
     wordLetters = wordLetters.map((l) => l.toLowerCase());
-    //fill states
+    //o useState pickedWord é prenchido com a constante word
     setPickedWord(word);
+    //o useState PickedCategory é prenchido com a constante category
     setPickedCategory(category);
+    //o useState letters é prenchido com a variável wordLetters
     setLetters(wordLetters);
-
+    //setGameStage no final pois é preciso primeiro selecionar a categoria,a letra e separá-la.
     setGameStage(stages[1].name);
   }, [pickWordAndCategory]);
 
-  //process the letter input
+  //função para processar as letras digitadas
   const verifyLetter = (letter) => {
+    //deixando a letra padronizada
     const normalizedLetter = letter.toLowerCase();
-    //check if letter has already been utilized
+    //condicional para averiguar se a letra já foi utilizada de alguma maneira. Desse modo o usuário não desperdiça uma guess(tentativa) e nem processa uma letra que ele ja adivinhou
     if (
       guessedLetters.includes(normalizedLetter) ||
       wrongLetters.includes(normalizedLetter)
     ) {
+      //encerra a função verifyLetter
       return;
     }
 
-    //push guessed letter or remove a guess
+    //condicional para incluir a letra jogada para as letras adivinhadas(corretas) ou para as letras erradas
+    //verifica se a letra jogada está inclusa em algumas das letras da palavra
     if (letters.includes(normalizedLetter)) {
+      //se a condição for verdadeira, o state de letra adivinhadas terá um array com as atuais letras adivinhadas mais a nova letra adivinhada
       setGuessedLetters((actualGuessedLetters) => [
         ...actualGuessedLetters,
         normalizedLetter,
       ]);
     } else {
+      //se a condição for falsa, o state de letra erradas terá um array com as atuais letras errada mais a nova letra jogada
       setWrongLetters((actualWrongLetters) => [
         ...actualWrongLetters,
         normalizedLetter,
       ]);
-
+      //além disso, será diminuida as tentativas
       setGuesses((actualGuesses) => actualGuesses - 1);
     }
   };
-
+  //função para limpar os states das letras adivinhadas e erradas
   const clearLettersStates = () => {
     setGuessedLetters([]);
     setWrongLetters([]);
   };
 
-  //check if guesses ended
+  //hook para monitorar alguma dado, nesse caso será o state das tentativas(guesses). Essa é a condição de derrota
   useEffect(() => {
+    //condição quando as tentativas forem igual ou menor a 0
     if (guesses <= 0) {
-      //reset all states
+      //funçao para limpar todas letras, erradas e adivinhadas
       clearLettersStates();
+      //direciona para tela de fim de jogo
       setGameStage(stages[2].name);
     }
   }, [guesses]);
 
-  //check win condition
+  //hook para monitorar as letras adivinhadas. Essa é a condição de vitória.
   useEffect(() => {
+    //constante de array de letras sem repetição da palavra selecionada
     const uniqueLetters = [...new Set(letters)];
-    //win condition
-    if (guessedLetters.length === uniqueLetters.length) {
-      // add score
+    //vitória quando o array das letras adivinhadas que vai sendo preenchido possui o mesmo tamanho do array das letras sem repetição que vem do letters, contudo, a condição só é verificada após o start do game, na parte do GameScreen.
+    if (
+      guessedLetters.length === uniqueLetters.length &&
+      gameStage === stages[1].name
+    ) {
+      //adiciona o score quando adivinha a palavra
       setScore((actualScore) => (actualScore += 100));
-      //restart game with new word
-      startGame();
+      //resetar o jogo com a nova palavra
+      startGame;
     }
-  }, [guessedLetters, letters, startGame]);
+  }, [guessedLetters, startGame, letters, gameStage]);
 
-  //restarts the game
+  //função para reniciar o jogo
   const retry = () => {
+    //a pontução só é limpada depois do retry pois ela precisa aparecer na tela de fim de jogo
     setScore(0);
+    //as tentativas da mesma forma da pontuação voltam para 3 após o retry
     setGuesses(guessesQty);
     setGameStage(stages[0].name);
   };
-
   return (
     <div className="App">
+      {/*O componente da tela inicial só é ativado se o gameStage for start*/}
       {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {/*O componente da tela do jogo só é ativado se o gameStage for game*/}
       {gameStage === "game" && (
         <GameScreen
           verifyLetter={verifyLetter}
@@ -134,6 +154,7 @@ const App = () => {
           score={score}
         />
       )}
+      {/*O componente da tela final só é ativado se o gameStage for end*/}
       {gameStage === "end" && <EndScreen retry={retry} score={score} />}
     </div>
   );
--- secret_word/secret_word/src/components/GameScreen.jsx
+++ secret_word/secret_word/src/components/GameScreen.jsx
@@ -3,7 +3,6 @@ import "./GameScreen.modules.css";
 
 const GameScreen = ({
   verifyLetter,
-  pickedWord,
   pickedCategory,
   letters,
   guessedLetters,
@@ -11,13 +10,17 @@ const GameScreen = ({
   wrongLetters,
   score,
 }) => {
+  //state para somente uma letra que será exibida
   const [letter, setLetter] = useState("");
+  //constante do objeto dom input selecionado
   const letterInputRef = useRef(null);
-
+  //a função handleSubmit ativa a função de verificar a letra que foi importada para o gameScreen, com o parâmetro letter
   const handleSubmit = (e) => {
     e.preventDefault();
     verifyLetter(letter);
+    //apaga o letter no state e no input
     setLetter("");
+    //hook para voltar a selecionar o input da letra após a letra ser envida no onSubmit
     letterInputRef.current.focus();
   };
   return (
@@ -27,30 +30,39 @@ const GameScreen = ({
       </p>
       <h1>Advinhe a palavra:</h1>
       <h3 className="tip">
+        {/*a dica é a categoria selecionada*/}
         Dica sobre a palavra: <span>{pickedCategory}</span>
       </h3>
       <p>Você ainda tem {guesses} tentativa(s).</p>
       <div className="wordContainer">
+        {/*map para formar a estrutura da palavra selecionada, parâmetros para a letra e o seu indice key*/}
         {letters.map((letter, i) =>
+          //se o array do state guessedLetter tiver nele uma letra da palavra selecionada, ela será impressa na tela
           guessedLetters.includes(letter) ? (
             <span key={i} className="letter">
               {letter}
             </span>
           ) : (
+            //se não será impresso um quadrado em branco, por isso o map é usado no state de letters, para aparecer os quadrado na tela conforme o tamanho da palavra
             <span key={i} className="blankSquare"></span>
           )
         )}
       </div>
       <div className="letterContainer">
         <p>Tente advinhar uma letra da palavra:</p>
+        {/*evento onSubmit para ativar a função handleSubmit*/}
         <form onSubmit={handleSubmit}>
           <input
             type="text"
             name="letter"
             maxLength="1"
+            /*o required permite que o form seja enviado somente após o preenchimento*/
             required
+            /*evento onChange para alterar o state letter conforme a letra que o usuário digitou*/
             onChange={(e) => setLetter(e.target.value)}
+            /*input vai ser preenchido pelo state*/
             value={letter}
+            /*o ref no input é como se letterInputRef fosse a constante do objeto dom input selecionado*/
             ref={letterInputRef}
           />
           <button>Jogar!</button>
@@ -58,6 +70,7 @@ const GameScreen = ({
       </div>
       <div className="wrongLettersContainers">
         <p>Letras já utilizadas:</p>
+        {/*Map para exibir as palavras já tentadas que foram erradas*/}
         {wrongLetters.map((letter, i) => (
           <span key={i}> {letter},</span>
         ))}
